f026eb96aa0d88b6c74acc101da4780bc5dfd61b
Gunavarrdhini2004/FSD <321506402298@andhrauniversity.edu.in>
2023-06-20 13:06:26 +0530

    adding new assignment files

---
+++ overloading.java
@@ -0,0 +1,15 @@
+class overloading
+{
+static int add(int a,int b)
+{
+return a+b;
+static int add(int a,int b,int c)
+{
+return a+b+c;
+}
+public static void main(String[] args)
+{
+system.out.println(overloading.add(18,9));
+system.out.println("overloading.add(3,7));
+}
+}
